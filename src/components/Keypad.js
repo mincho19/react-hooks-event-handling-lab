296b8797d83fdf0f0eac70b8b80912b915949210
Min Cho <minsangcho19@gmail.com>
2022-01-12 12:07:48 -0800

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+function Keypad(){
+    function handleClick(){
+        console.log('Entering password...')
+    }
+    
+    return <input onChange={handleClick} type="password"></input>
+    
+}
+
+export default Keypad
